5bb3b68b09b224ba0571f69477c2c6b53e160424
Sam <student979@live.com>
2019-10-20 18:30:54 -0400

    Task: added support for adding numbers

--- src/calcTest.py
+++ src/calcTest.py
@@ -1,3 +1,6 @@
-num1 = input("enter a number: ")
+num1 = float(input("enter a number: "))
 op = input("enter an operation: ")
-num2 = input("enter a number: ")
+num2 = float(input("enter a number: "))
+
+if op=="+":
+    print(str(num1)+" + "+ str(num2) + " = " +str((num1+num2)))
