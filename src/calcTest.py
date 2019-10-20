f8546799ff75bfd487d8ca416802fe22393dd108
Sam <student979@live.com>
2019-10-20 18:32:44 -0400

    Fix: linting

--- src/calcTest.py
+++ src/calcTest.py
@@ -2,7 +2,7 @@ num1 = float(input("enter a number: "))
 op = input("enter an operation: ")
 num2 = float(input("enter a number: "))
 
-if op=="+":
-    print(str(num1)+" + "+ str(num2) + " = " +str((num1+num2)))
-elif op=="-":
-    print(str(num1)+" - "+ str(num2) + " = " +str((num1-num2)))
+if op == "+":
+    print(str(num1) + " + " + str(num2) + " = " + str((num1 + num2)))
+elif op == "-":
+    print(str(num1) + " - " + str(num2) + " = " + str((num1 - num2)))
