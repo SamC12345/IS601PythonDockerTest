5337feab60cb54a17fb6a78e8905366666ce27af
Sam <student979@live.com>
2019-10-20 18:34:26 -0400

    Task: added division support

--- src/calcTest.py
+++ src/calcTest.py
@@ -8,3 +8,8 @@ elif op == "-":
     print(str(num1) + " - " + str(num2) + " = " + str((num1 - num2)))
 elif op == "*":
     print(str(num1) + " * " + str(num2) + " = " + str((num1 * num2)))
+elif op == "/":
+    if num2 == 0:
+        print("Error: Denominator must not equal 0")
+    else:
+        print(str(num1) + " / " + str(num2) + " = " + str((num1 / num2)))
